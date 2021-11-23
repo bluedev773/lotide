7592daa5e0c8553d6fab8977117a65d555d9b979
Matthew Litvinov <matthew.litvinov@gmail.com>
2021-11-23 17:27:40 -0500

    remove unwanted elements in array

---
+++ without.js
@@ -0,0 +1,34 @@
+const eqArrays = function(arrOne, arrTwo) {
+
+  //Check if arrays are the same length
+  if(arrOne.length !== arrTwo.length) {
+    return false;
+  }
+
+  //Check if the elements at each index are the same
+  for(let i = 0; i < arrOne.length; i++){
+      if(arrOne[i] !== arrTwo[i]){
+        return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(arrOne, arrTwo) {
+  if (eqArrays(arrOne, arrTwo) === true) {
+    console.log(`✅✅✅ Assertion Passed: Array One  ===  Array Two`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: Array One !==  Array Two`);
+  }
+}
+
+const without = function (source, itemsToRemove) {
+ output = source.filter(item => !itemsToRemove.includes(item));
+ console.log(output);
+}
+
+//Test Cases
+const words = ["hello", "world", "lighthouse"];
+without(words, ["lighthouse"]); // no need to capture return value for this test case
+// Make sure the original array was not altered by the without function
+assertArraysEqual(words, ["hello", "world", "lighthouse"]);
