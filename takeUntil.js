ce4063f6f43828740c9762ff0147508b5e821256
Matthew Litvinov <matthew.litvinov@gmail.com>
2021-11-25 16:39:34 -0500

    slice array from elements at beginning until callback returns true

---
+++ takeUntil.js
@@ -0,0 +1,46 @@
+const eqArrays = function(arrOne, arrTwo) {
+
+  //Check if arrays are the same length
+  if(arrOne.length !== arrTwo.length) {
+    return false;
+  }
+
+  //Check if the elements at each index are the same
+  for(let i = 0; i < arrOne.length; i++){
+      if(arrOne[i] !== arrTwo[i]){
+        return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(arrOne, arrTwo) {
+  if (eqArrays(arrOne, arrTwo) === true) {
+    console.log(`✅✅✅ Assertion Passed: Array One: ${arrOne}  ===  Array Two: ${arrTwo}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: Array One: ${arrOne} !==  Array Two: ${arrTwo}`);
+  }
+}
+
+const takeUntil = function(array, callback) {
+  const results = [];
+  for(let item of array){
+    if(!callback(item)){
+      results.push(item)
+    } else {
+      return results;
+    }
+  }
+}
+
+
+//Test cases
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+assertArraysEqual(results1,[1,2,5,7,2]);
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+assertArraysEqual(results2, [ 'I\'ve', 'been', 'to', 'Hollywood' ]);
