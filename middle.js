7f89d00db6e0c9f7aec0b92b21ca43c506f488f1
Matthew Litvinov <matthew.litvinov@gmail.com>
2021-11-29 14:12:06 -0500

    refactor middle to modularize and to seperate test cases

--- middle.js
+++ middle.js
@@ -1,26 +1,3 @@
-const eqArrays = function(arrOne, arrTwo) {
-
-  //Check if arrays are the same length
-  if(arrOne.length !== arrTwo.length) {
-    return false;
-  }
-
-  //Check if the elements at each index are the same
-  for(let i = 0; i < arrOne.length; i++){
-      if(arrOne[i] !== arrTwo[i]){
-        return false;
-    }
-  }
-  return true;
-};
-
-const assertArraysEqual = function(arrOne, arrTwo) {
-  if (eqArrays(arrOne, arrTwo) === true) {
-    console.log(`✅✅✅ Assertion Passed: Array One  ===  Array Two`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: Array One !==  Array Two`);
-  }
-}
 
 const middle = function(arr) {
   const output = [];
@@ -35,7 +12,4 @@ const middle = function(arr) {
   return output;
 }
 
-//Test cases
-assertArraysEqual(middle([1]),[]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]),[3,4]);
+module.exports = middle;
